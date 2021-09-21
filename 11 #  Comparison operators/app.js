89f481aafcbeb063f961c2b3337574f105319086
usami23 <usami230@gmail.com>
2021-09-21 11:05:40 -0700

    11 & 12

---
+++ 11 #  Comparison operators/app.js
@@ -0,0 +1,50 @@
+// 11 Comparison Operators
+
+// == equal to  Return True OR False    
+
+// === equal value and equal type    Return True OR False 
+
+// != not equal Return True OR False 
+
+// !== not equal value or not equal type   Return True OR False
+
+// > greater than Return True OR False
+
+// < less than  Return True OR False
+
+// >= greater than or equal to  Return True OR False
+
+// <= less than or equal to   Return True OR False
+
+var x = 5;
+//console.log(x == 5);
+//console.log(x == 3);
+
+// console.log(x === 5)
+// console.log(x === '5')
+
+// console.log(x != 5)
+// console.log(x != 4)
+
+// console.log(x !== 5);
+// console.log(x !== 4);
+// console.log(x !== '4');
+// console.log(x !== '5');
+
+
+// console.log(x > 4);
+// console.log(x > 6);
+// console.log(x > 5)
+
+// console.log(x < 6)
+// console.log(x < 4)
+// console.log(x < 5)
+
+//  console.log(x >= 5)
+//  console.log(x >= 4)
+//  console.log(x >= 6)
+
+
+//  console.log(x <= 5)
+//  console.log(x <= 4)
+//  console.log(x <= 6)
---
+++ 12 # if...else and else if statements/app.js
@@ -0,0 +1,32 @@
+// 12 Chapter if else Statement
+
+// Use if to specify a block of code to be executed, if a specified condition is true
+// Use else to specify a block of code to be executed, if the same condition is false
+// Use else if to specify a new condition to test, if the first condition is false
+
+
+// if (condition1) {
+//     //  block of code to be executed if condition1 is true
+//   } else if (condition2) {
+//     //  block of code to be executed if the condition1 is false and condition2 is true
+//   } else {
+//     //  block of code to be executed if the condition1 is false and condition2 is false
+//   }
+
+// var x = 10;
+// if (x < 10) {
+//     console.log("First Condition");
+//   } else if (x < 20) {
+//     console.log("Second Condition");
+//   } else {
+//     console.log("Else Condition");
+//   }
+
+// var letter = "e";
+// if (letter === "c") {
+//     console.log("First Condition");
+//   } else if (letter === "b" || letter === "d") {
+//     console.log("Second Condition");
+//   } else {
+//     console.log("Else Condition");
+//   }
