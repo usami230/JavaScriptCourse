4fabeaaa8a64fe761bffb1950daf8c6585c0fe95
IT Samiuddin <sami@pearlfood.com>
2021-09-22 13:43:57 +0500

    15 Chapter

---
+++ 15 # Arrays/app.js
@@ -0,0 +1,36 @@
+// Chapter 15 Arrays
+
+// JavaScript arrays are used to store multiple values in a single variable.
+
+// An array is a special variable, which can hold more than one value at a time.
+
+// var array_name = [item1, item2, ...];   
+
+// array index start = 0
+
+// array length start = 1
+
+// var city0 = "Atlanta";
+// var city1 = "Baltimore"; 
+// var city2 = "Chicago"; 
+// var city3 = "Denver"; 
+// var city4 = "Los Angeles"; 
+// var city5 = "Seattle";
+
+//console.log("Welcome To : " + city3);
+
+//var cities = [city0,city1,city3]
+
+//console.log(cities.length);
+
+//console.log(cities);
+
+//  var cities = ["Atlanta","Denver","Seattle"];
+//  console.log(cities);
+
+//  // The toString() method returns an array as a comma separated string:
+//  console.log(cities.toString());
+
+// var mixedArray = [1, "Bob", "Now is", true];
+// console.log(mixedArray);
+// console.log(mixedArray[1]);
