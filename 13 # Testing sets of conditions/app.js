67c938b7df397b69b1625c74f10379d3ccb1876a
IT Samiuddin <sami@pearlfood.com>
2021-09-22 13:07:30 +0500

    Chap 12

---
+++ 13 # Testing sets of conditions/app.js
@@ -0,0 +1,35 @@
+// Chap 13 Testing sets of conditions
+
+var weight = 300;
+var time = 4;
+var gender = "Male";
+var age = 18;
+
+// if( weight > 300 && time < 6)
+// {
+//     alert("True"); 
+// }
+// else{
+//     alert("false"); 
+// }
+
+// if( weight >= 300 && time < 5)
+// {
+//     alert("True"); 
+// }
+// else{
+//     alert("false"); 
+// }
+
+// if (weight >= 300 && time < 6 && age > 17 && gender === "male") { 
+//     alert("True"); 
+// }
+// else{
+//     alert("false"); 
+// }
+// if (weight >= 300 && time < 6 && age > 17 && gender === "Male") { 
+//     alert("True"); 
+// }
+// else{
+//     alert("false"); 
+// }
