57bcb55aed33eb2263d32ce087c07ea445616164
IT Samiuddin <sami@pearlfood.com>
2021-09-22 15:32:57 +0500

    16 & 17

---
+++ 16 # Arrays Adding and removing elements/app.js
@@ -0,0 +1,36 @@
+// Chapter 16 Arrays: Adding and removing elements
+ 
+var pets = [];
+
+pets[0] = "dog"; 
+pets[1] = "cat";
+pets[2] = "bird";
+
+// pop() = Using the keyword, pop, you can remove the last element of an array. 
+
+// pets[3] = "lizard";
+
+// console.log(pets);
+
+// console.log(pets.pop())
+
+// // pop() returned deleted value
+
+// console.log(pets);
+
+// var cities = ["Karachi","Lahore"];
+// cities[10] = "Pindi";
+
+// console.log(cities);
+// console.log(cities[6]); 
+
+// if array index empty return undefined
+
+// push() Using the keyword, push, you can add one or more elements to the end of an array. 
+
+
+var courses = ["Java","C#"];
+console.log(courses);
+courses.push("Python");
+console.log(courses);
+
