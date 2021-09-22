d2df7171bea45d0c31f3e305ec1742c9cdffa370
IT Samiuddin <sami@pearlfood.com>
2021-09-22 13:30:03 +0500

    Chap 14

---
+++ 14 # if statements nested/app.js
@@ -0,0 +1,58 @@
+// Chap 14 if statement nested
+
+// if (condition1) 
+// {
+//    // Executes when condition1 is true
+//    if (condition2) 
+//    {
+//       // Executes when condition2 is true
+//    }
+// }
+
+// var country = "India";
+// var city = "Bombay";
+// var area = "Def";
+
+// if (country == "Pakistan") {
+//     console.log("Country name :" + country)
+//     if (city == "Karachi") {
+//         console.log("City Name :" + city);
+//         if (area == "Saddar" || area == "Shah Faisal" || area == "Malir") {
+//             console.log("Area Name :" + area);
+//         }
+//         else {
+//             console.log("Invalid Area");
+//         }
+//     }
+//     else if (city == "Hyderabad") {
+//         console.log("City Name :" + city);
+//         if (area == "Pakka Qila") {
+//             console.log("Area Name :" + area);
+//         }
+//         else {
+//             console.log("Invalid Area");
+//         }
+
+//     }
+//     else {
+//         console.log("Invalid City");
+//     }
+// }
+// else if (country == "India") {
+//     console.log("Country name :" + country)
+//     if (city == "Bombay") {
+//         console.log("City Name :" + city);
+//         if (area == "Abc" || area == "Def") {
+//             console.log("Area Name :" + area);
+//         }
+//         else {
+//             console.log("Invalid Area");
+//         }
+//     }
+//     else {
+//         console.log("Invalid City");
+//     }
+// }
+// else {
+//     console.log("Invalid Country");
+// }
